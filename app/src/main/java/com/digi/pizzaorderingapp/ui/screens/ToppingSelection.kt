8b5d278e5a222c5bdbbd23c8f04465641022c096
Zaid Kamil <xaidmetamorphos@gmail.com>
2024-07-30 16:06:45 +0530

    Topping Selection

--- app/src/main/java/com/digi/pizzaorderingapp/ui/screens/ToppingSelection.kt
+++ app/src/main/java/com/digi/pizzaorderingapp/ui/screens/ToppingSelection.kt
@@ -1,5 +1,6 @@
 package com.digi.pizzaorderingapp.ui.screens
 
+import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -14,6 +15,10 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -25,8 +30,13 @@ import com.digi.pizzaorderingapp.dummyPizzaRepository
 import com.digi.pizzaorderingapp.topicRepository
 
 @Composable
-fun ToppingSelectionScreen(modifier: Modifier = Modifier, pizzaId: Int) {
+fun ToppingSelectionScreen(
+    modifier: Modifier = Modifier,
+    pizzaId: Int,
+    onConfirm: (Set<Topping>) -> Unit = {},
+) {
     val pizza = dummyPizzaRepository()[pizzaId]
+    var selectedToppings by remember { mutableStateOf(emptySet<Topping>()) }
     Box(
         modifier = modifier
             .fillMaxSize()
@@ -45,7 +55,14 @@ fun ToppingSelectionScreen(modifier: Modifier = Modifier, pizzaId: Int) {
                 contentPadding = PaddingValues(vertical = 16.dp),
             ) {
                 items(topicRepository()) {
-                    ToppingItem(topping = it)
+                    ToppingItem(topping = it) { selection ->
+                        if (selection.isChecked) {
+                            selectedToppings = selectedToppings.plusElement(selection)
+                        } else {
+                            selectedToppings = selectedToppings.minusElement(selection)
+                        }
+                        Log.d("Toppings selected", "$selectedToppings")
+                    }
                 }
             }
         }
@@ -53,7 +70,12 @@ fun ToppingSelectionScreen(modifier: Modifier = Modifier, pizzaId: Int) {
 }
 
 @Composable
-fun ToppingItem(modifier: Modifier = Modifier, topping: Topping) {
+fun ToppingItem(
+    modifier: Modifier = Modifier,
+    topping: Topping,
+    onToppingSelected: (Topping) -> Unit,
+) {
+    var selectedCheckbox by remember { mutableStateOf(false) }
     Card(
         modifier = modifier
             .fillMaxWidth()
@@ -63,8 +85,13 @@ fun ToppingItem(modifier: Modifier = Modifier, topping: Topping) {
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier.padding(end = 16.dp)
         ) {
-            Checkbox(checked = topping.isChecked,
-                onCheckedChange = { topping.isChecked = it }
+            Checkbox(checked = selectedCheckbox,
+                onCheckedChange = {
+                    Log.d("Checkbox value", "$it")
+                    selectedCheckbox = it
+                    topping.isChecked = selectedCheckbox
+                    onToppingSelected(topping)
+                }
             )
             Text(text = topping.title, fontWeight = FontWeight.Bold)
             Spacer(modifier = Modifier.weight(1f))
@@ -76,7 +103,7 @@ fun ToppingItem(modifier: Modifier = Modifier, topping: Topping) {
 @Preview
 @Composable
 private fun ToppingItemPreview() {
-    ToppingItem(topping = Topping("Cheese", 100.00))
+    ToppingItem(topping = Topping("Cheese", 100.00)) {}
 }
 
 @Preview(showSystemUi = true)
